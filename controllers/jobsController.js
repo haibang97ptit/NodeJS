5ddf45847de2e799e6a0306d424adb620812dda7
haibang97ptit <37463730+haibang97ptit@users.noreply.github.com>
2021-07-07 09:48:02 +0700

    update and delete

--- controllers/jobsController.js
+++ controllers/jobsController.js
@@ -1,10 +1,13 @@
 const Job = require("../models/jobs");
 
 //get all jobs => api/v1/jobs
-exports.getJobs = (req, res, next) => {
+exports.getJobs = async (req, res, next) => {
+  const jobs = await Job.find();
+
   res.status(200).json({
     success: true,
-    message: "Get all jobs in here",
+    result: jobs.length,
+    data: jobs,
   });
 };
 
@@ -16,5 +19,45 @@ exports.newJob = async (req, res, next) => {
     succes: true,
     message: "Job created",
     data: job,
+  });
+};
+
+exports.updateJob = async (req, res, next) => {
+  let job = await Job.findById(req.params.id);
+
+  if (!job) {
+    return res.status(404).json({
+      success: false,
+      message: "Job not found",
+    });
+  }
+
+  job = await Job.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+    runValidators: true,
+    useFindAndModify: false,
+  });
+
+  res.status(200).json({
+    success: true,
+    message: "Job is updated",
+    data: job,
+  });
+};
+
+exports.deleteJob = async (req, res, next) => {
+  let job = await Job.findByIdAndDelete(req.params.id);
+
+  if (!job) {
+    return res.status(404).json({
+      success: false,
+      message: "Job not found",
+    });
+  }
+
+  job = await Job.findByIdAndDelete(req.params.id);
+  res.status(200).json({
+    success: true,
+    message: "Job is deleted",
   });
 };
--- route/jobs.js
+++ route/jobs.js
@@ -1,9 +1,15 @@
 const express = require("express");
 const router = express.Router();
 
-const { getJobs, newJob } = require("../controllers/jobsController");
+const {
+  getJobs,
+  newJob,
+  updateJob,
+  deleteJob,
+} = require("../controllers/jobsController");
 
 router.route("/jobs").get(getJobs);
 router.route("/job/new").post(newJob);
+router.route("/job/:id").put(updateJob).delete(deleteJob);
 
 module.exports = router;
